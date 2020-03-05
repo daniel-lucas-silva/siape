8871c3eae1ac3cfaef5fd830e7bd7823c3052e8c
Daniel Lucas de Souza Silva <dluc18@gmail.com>
2020-03-05 04:36:58 -0300

    Updates

---
+++ precache-manifest.bde783e6a1f26edcf821d0f046e2f7eb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5aaa5fdfbca5dad484e14c17960442",
+    "url": "/siape/index.html"
+  },
+  {
+    "revision": "9aa6977e2949958d162f",
+    "url": "/siape/static/css/2.b112cd55.chunk.css"
+  },
+  {
+    "revision": "d44b39291332d1556338",
+    "url": "/siape/static/css/main.e597c7df.chunk.css"
+  },
+  {
+    "revision": "9aa6977e2949958d162f",
+    "url": "/siape/static/js/2.bb878db6.chunk.js"
+  },
+  {
+    "revision": "3e7eb0c51ce474724fe2295510ac9693",
+    "url": "/siape/static/js/2.bb878db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44b39291332d1556338",
+    "url": "/siape/static/js/main.ae622ea7.chunk.js"
+  },
+  {
+    "revision": "ce4677b7728417619384",
+    "url": "/siape/static/js/runtime-main.68db6d77.js"
+  },
+  {
+    "revision": "1dfb73e22b9119f547fb880568f56ea2",
+    "url": "/siape/static/media/Framework7Icons-Regular.1dfb73e2.woff2"
+  },
+  {
+    "revision": "3b973a05c0e1544f7ee8fa23c2a9315a",
+    "url": "/siape/static/media/Framework7Icons-Regular.3b973a05.ttf"
+  },
+  {
+    "revision": "535bcf7dd3feb41ec20507c3c5b81efb",
+    "url": "/siape/static/media/Framework7Icons-Regular.535bcf7d.woff"
+  },
+  {
+    "revision": "9ee96e3a07dec6235d3f1a86787a92b7",
+    "url": "/siape/static/media/Framework7Icons-Regular.9ee96e3a.eot"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/siape/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/siape/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/siape/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/siape/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
